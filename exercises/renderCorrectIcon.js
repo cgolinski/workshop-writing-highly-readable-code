bf4544ed54692995c3b498b9bad1ad6c2f8eb2c2
cgolinski <cgolinski@users.noreply.github.com>
2023-06-27 09:21:21 -0400

    Update renderCorrectIcon.js

--- exercises/renderCorrectIcon.js
+++ exercises/renderCorrectIcon.js
@@ -2,7 +2,7 @@ const renderCorrectIcon = (subject) => {
   let subjectType = getSubjectType(subject);
   if (subjectType === "math") {
     return <MathIcon />;
-  } else if (studentType === "arts") {
+  } else if (subjectType === "arts") {
     return <ArtsIcon />;
   } else {
     return <HistoryIcon />;
